733e480b70e1d87ef30dc2472f9fdf610fa09adb
Nedra Krifa <nedrakrifa@gmail.com>
2020-09-10 21:50:59 +0200

    Updates

---
+++ precache-manifest.8976e05fb1a531763ff145b571a14e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a004fd1d00fdde5767b6ecc0967685bf",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "0c3e58242b7fc3619d88",
+    "url": "/react-movie-app/static/css/main.acb0222f.chunk.css"
+  },
+  {
+    "revision": "db292c36b78eafdf2a0a",
+    "url": "/react-movie-app/static/js/2.df5e1192.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.df5e1192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3e58242b7fc3619d88",
+    "url": "/react-movie-app/static/js/main.4a72f4f3.chunk.js"
+  },
+  {
+    "revision": "1ed08b8a2033de2870fc",
+    "url": "/react-movie-app/static/js/runtime-main.5ed8f7af.js"
+  },
+  {
+    "revision": "3990f5ad5aa58a79d2cf57bfd0a1b36d",
+    "url": "/react-movie-app/static/media/background.3990f5ad.png"
+  }
+]);
